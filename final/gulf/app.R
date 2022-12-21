b19a0cf714837ab9a0bf53319afff5ad2eb2cade
natsuki221 <linnatsuki221@gmail.com>
2022-12-21 23:06:00 +0800

    V3.1.0
    * append Question 6

--- final/gulf/app.R
+++ final/gulf/app.R
@@ -12,31 +12,38 @@ gulf.no.view = na.omit(gulf.no.view)
 
 #At 95% confidence, alpha = 0.05 and alpha/2 = 0.025
 #t(0.025) is based on n-1 == 40 - 1 = 39 degrees of freedom.
-gulf.v.t.val = round(qt(0.025, 39, lower.tail = F), 3)
-
-#interval estimate --> xBar +- t(0.025) * s / sqrt(n)
-gulf.v.mean <- mean(gulf.view$`Sale Price`)
-gulf.v.sd <- sd(gulf.view$`Sale Price`)
-gulf.v.n <- nrow(gulf.view)
-gulf.v.in.es = gulf.v.t.val * gulf.v.sd / sqrt(gulf.v.n)
-gulf.v.posi.in.es = round((gulf.v.mean + gulf.v.in.es) * 1000, 2)
-gulf.v.nega.in.es = round((gulf.v.mean - gulf.v.in.es) * 1000, 2)
-
-#margin of error E = z(alpha/2) * sd / sqrt(n), so n = (E / z(alpha/2) * sd) ^ 2
-gulf.v.z = qnorm(0.05/2, lower.tail = F)
-gulf.v.nss = round((gulf.v.z * gulf.v.sd / gulf.v.in.es) ** 2)
+v.t.val = round(qt(0.025, 39, lower.tail = F), 3)
 
 #At 95% confidence, alpha = 0.05 and alpha/2 = 0.025
 #t(0.025) is based on n-1 == 18 - 1 = 17 degrees of freedom.
 gulf.nv.t.val = round(qt(0.025, 17, lower.tail = F), 3)
 
 #interval estimate --> xBar +- t(0.025) * s / sqrt(n)
-gulf.nv.mean <- mean(gulf.no.view$`Sale Price`)
-gulf.nv.sd <- sd(gulf.no.view$`Sale Price`)
-gulf.nv.n <- nrow(gulf.no.view)
-gulf.nv.in.es = gulf.nv.t.val * gulf.nv.sd / sqrt(gulf.nv.n)
-gulf.nv.posi.in.es = round((gulf.nv.mean + gulf.nv.in.es) * 1000, 2)
-gulf.nv.nega.in.es = round((gulf.nv.mean - gulf.nv.in.es) * 1000, 2)
+interval.estimate <- function(m){
+  interval <- c(NA, NA)
+  mean <- mean(m)
+  sd <- sd(m)
+  n = length(m)
+  t.val = qt(0.025, n-1, lower.tail = F)
+  in.es <- t.val * sd / sqrt(n)
+  interval[1] = round(mean - in.es, 2)
+  interval[2] = round(mean + in.es, 2)
+  
+  return(interval)
+}
+
+################################--Question4--###################################
+v.price.interval = interval.estimate(gulf.view$`Sale Price`)
+v.days.of.sell = interval.estimate(gulf.view$`Days to Sell`)
+
+################################--Question5--###################################
+nv.price.interval = interval.estimate(gulf.no.view$`Sale Price`)
+nv.days.of.sell = interval.estimate(gulf.no.view$`Days to Sell`)
+
+################################--Question6--###################################
+#margin of error E = z(alpha/2) * sd / sqrt(n), so n = (E / z(alpha/2) * sd) ^ 2
+v.z = qnorm(0.05/2, lower.tail = F)
+v.nss = round((gulf.v.z * gulf.v.sd / gulf.v.in.es) ** 2)
 
 #margin of error E = z(alpha/2) * sd / sqrt(n), so n = (E / z(alpha/2) * sd) ^ 2
 gulf.nv.z = qnorm(0.05/2, lower.tail = F)
